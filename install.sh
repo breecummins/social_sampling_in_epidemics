d0775e5d3777d4ae270c136b1832fdb3ab287347
Bree Cummins <breecummins@gmail.com>
2022-03-18 13:26:11 -0600

    testing new installation

--- install.sh
+++ install.sh
@@ -10,18 +10,21 @@ git clone git@github.com:veg/tn93.git
 cd tn93
 cmake .
 make install
-cd ../..
-
-# grab the python CCM sampler and copy locally
-wget https://raw.githubusercontent.com/ravigoyalgit/CCMnet_py/master/inst/python/CCMnet_constr_py.py src/social_epi/
+cd ..
+# rm -rf tn93
 
 # install FAVITES
-cd scripts
 wget https://raw.githubusercontent.com/niemasd/FAVITES/master/run_favites_docker.py 
 chmod a+x run_favites_docker.py
 wget https://raw.githubusercontent.com/niemasd/FAVITES/master/helper_scripts/tn93_to_clusters.py 
 chmod a+x tn93_to_clusters.py
 cd ..
+
+# grab the python CCM sampler and copy locally
+cd src/social_epi
+wget https://raw.githubusercontent.com/ravigoyalgit/CCMnet_py/master/inst/python/CCMnet_constr_py.py src/social_epi/
+cd ../..
+
 
 # install package
 pip uninstall -y social_epi &> /dev/null || True
