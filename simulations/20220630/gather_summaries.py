5ab2c1a3bd59f743b1c2a191c8d954b8bbd51f93
Bree Cummins <breecummins@gmail.com>
2022-07-01 14:00:57 -0600

    added new naming convention for summarized data

--- simulations/20220630/gather_summaries.py
+++ simulations/20220630/gather_summaries.py
@@ -1,17 +1,12 @@
-import os
+import sys
 import subprocess
 import pandas as pd
 
 files = subprocess.check_output('find "$PWD" | grep "summary"',shell=True).splitlines()
-#filelist = files.decode("utf-8").split("\n")
-#filelist = subprocess.run('find "$PWD" | grep "summary"', shell=True,stdout=PIPE).stdout.splitlines()
 filelist = [i.decode("utf-8") for i in files]
-print(len(files))
-print(len(filelist))
 master_df = pd.DataFrame()
 for f in filelist:
     df = pd.read_csv(f)
     master_df = pd.concat([master_df,df])
-    #print(len(master_df.index))
-master_df.to_csv("all_summaries.csv",index=False)
+master_df.to_csv("all_summaries_{}.csv".format(sys.argv[1]),index=False)
 
