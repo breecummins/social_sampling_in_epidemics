7dae2c7c5f678cb4d83fa5a11eef184a10326332
Bree Cummins <breschine.cummins@montana.edu>
2022-07-01 13:43:33 -0600

    added summary results for new simulations

--- simulations/20220630/gather_summaries.py
+++ simulations/20220630/gather_summaries.py
@@ -2,12 +2,16 @@ import os
 import subprocess
 import pandas as pd
 
-files = subprocess.check_output('find "$PWD" | grep "summary"',shell=True)
-filelist = files.decode("utf-8").split("\n")
+files = subprocess.check_output('find "$PWD" | grep "summary"',shell=True).splitlines()
+#filelist = files.decode("utf-8").split("\n")
+#filelist = subprocess.run('find "$PWD" | grep "summary"', shell=True,stdout=PIPE).stdout.splitlines()
+filelist = [i.decode("utf-8") for i in files]
+print(len(files))
+print(len(filelist))
 master_df = pd.DataFrame()
 for f in filelist:
-    if f:
-        df = pd.read_csv(f)
-        master_df = pd.concat([master_df,df])
+    df = pd.read_csv(f)
+    master_df = pd.concat([master_df,df])
+    #print(len(master_df.index))
 master_df.to_csv("all_summaries.csv",index=False)
 
