d3af579aa86d5eaba3112493dfef624b474a4fb8
Bree Cummins <breecummins@gmail.com>
2022-07-20 10:10:26 -0600

    trouble-shooting

--- simulations/20220713_study_params/trim_results.sh
+++ simulations/20220713_study_params/trim_results.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-cp results results_trimmed
+cp -r results results_trimmed
 cd results_trimmed/JOB*
+echo $PWD
 
 for dir in `ls`
 do
-   cd dir
-   cp FAVITES_output*/contact_network.txt .
-   cp FAVITES_output*/error_free_files/transmission_network.txt .
-   rm initial*
-   rm overlap*
-   rm *.json
-   rm -r FAVITES_output*/
-   cd ..
+   echo dir
 done
+
+# for dir in `ls`
+# do
+#    cd dir
+#    cp FAVITES_output*/contact_network.txt .
+#    cp FAVITES_output*/error_free_files/transmission_network.txt .
+#    rm initial*
+#    rm overlap*
+#    rm *.json
+#    rm -r FAVITES_output*/
+# done
 
 
