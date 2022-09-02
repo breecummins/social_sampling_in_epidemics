180fdee4378455bd92db7ae20c04e02dd572409d
Bree Cummins <breecummins@gmail.com>
2022-09-02 13:57:18 -0600

    altered results aggregation to include correct gemf files to recover compartment status

--- simulations/study_params_20220822/trim_results.sh
+++ simulations/study_params_20220822/trim_results.sh
@@ -8,7 +8,9 @@ do
    cd "$dir"
    cp FAVITES_output*/contact_network.txt .
    cp FAVITES_output*/error_free_files/transmission_network.txt .
-   cp FAVITES_output*/GEMF_output/status.txt compartment_status.txt
+   cp FAVITES_output*/GEMF_output/output.txt gemf_compartment_transitions.txt
+   cp FAVITES_output*/GEMF_output/gemf2orig.json .
+   cp FAVITES_output*/GEMF_output/README.TXT gemf_README.txt
    python ../../../modify_status.py $PWD
    rm initial*
    rm overlap*
