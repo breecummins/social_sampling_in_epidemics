781daa5a73ed3e774ebaf562514fad86f4cba1d1
Bree Cummins <breecummins@gmail.com>
2022-10-07 14:24:30 -0600

    changed model so that out-of-care goes to treated chronic rather than untreated chronic

--- simulations/study_params_20220929/check_incidence_swaps_transitiontime/run.py
+++ simulations/study_params_20220929/check_incidence_swaps_transitiontime/run.py
@@ -44,36 +44,4 @@ if __name__ == "__main__":
     statement2 = "Incidence mean +/- std: {:.03f} +/- {:.03f}".format(np.mean(incidence),np.std(incidence))
     print(statement2)
     open("summary.txt","w").write("\n".join([statement1,statement2]))
-
-    # Output of run 09/29/2022
-    """
-    Raw incidence: ['0.017', '0.017', '0.017', '0.015', '0.014', '0.015', '0.015', '0.016', '0.015', '0.014']
-    Incidence mean +/- std: 0.016 +/- 0.001
-    """
-
-    os.system("python ../../scripts/modify_status.py FAVITES_output/GEMF_output")
-
-    # Output of run 09/29/2022
-    """
-    Distribution of number of swaps: [(1, 1829), (2, 1422), (3, 651), (4, 144), (5, 15), (6, 6)]
-    Number (non-isolated) infected individuals: 4067
-    Mean and std of time from acute to chronic in years: 0.09783230769230769 +/- 0.11025268638145162
-    Compartment distribution of (non-isolated) infected individuals at simulation end:
-    Treated chronic      2470
-    Untreated chronic    1583
-    Untreated acute        10
-    Treated acute           4
-    """
-
-    # Output of second run 09/29/2022
-    """
-    Distribution of number of swaps: [(1, 1851), (2, 1422), (3, 555), (4, 96), (5, 15), (6, 6)]
-    Number (non-isolated) infected individuals: 3945
-    Mean and std of time from acute to chronic in years: 0.10955319871794873 +/- 0.11525989721382959
-    Compartment distribution of (non-isolated) infected individuals at simulation end:
-    Treated chronic      2405
-    Untreated chronic    1524
-    Untreated acute        16
-    """
-
-
+    
